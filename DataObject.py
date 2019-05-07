6b1a69d7aca981c8a6dee0aac9c47654c9eb00b6
OtimusOne <otimusone@yahoo.com>
2019-05-07 23:53:39 +0300

    DataObject class

---
+++ DataObject.py
@@ -0,0 +1,39 @@
+import os
+import stat
+import time
+import hashlib
+
+
+class DataObject:
+    def __init__(self, path):
+        try:
+            f = open(path, "rb")
+        except IOError:
+            print("Error: invalid file/path")
+            exit(1)
+
+        file_stats = os.stat(path)
+        self.name = f.name.split("\\")[-1]
+        self.path = f.name
+        self.size = file_stats[stat.ST_SIZE]
+        self.last_modified = time.strftime("%Y/%m/%d %I:%M:%S %p", time.localtime(file_stats[stat.ST_MTIME]))
+        self.data = f.read()
+        self.md5 = hashlib.md5(self.data).hexdigest()
+        self.type = None
+        f.close()
+
+    def printDataObject(self):
+        print("Name: ", self.name, "\nPath: ", self.path, "\nSize: ", self.size, "\nLast modified: ",
+              self.last_modified, "\nData: ", self.data, "\nMD5: ", self.md5, "\nType: ",
+              self.type, '\n', )
+
+
+"""
+def main():
+    x = input("Enter path to file: ")
+    o = DataObject(x)
+    o.printDataObject()
+
+
+main()
+"""
